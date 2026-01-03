d38d04df466454f0201c42b10bb3413accaaba43
riyabihani <riya.bihanii@gmail.com>
2026-01-03 23:16:47 +0530

    working on the filtering

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
 
                 {/* Links to Different Shopping Sections */}
                 <div className='hidden md:flex space-x-6'>
-                    <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Men</Link>
+                    <Link to='/collections/all' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Men</Link>
                     <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Women</Link>
                     <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Top Wear</Link>
                     <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Bottom Wear</Link>
