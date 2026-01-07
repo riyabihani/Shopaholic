69784949a7e72f36fb85b01ab221dabd6975b4b1
riyabihani <riya.bihanii@gmail.com>
2026-01-07 12:57:14 +0530

    finished admin layout

--- frontend/src/components/Admin/EditProductPage.jsx
+++ frontend/src/components/Admin/EditProductPage.jsx
@@ -29,11 +29,21 @@ const EditProductPage = () => {
         setProductData((prevData) => ({...prevData, [name]: value}));
     };
 
+    const handleImageUpload = async (e) => {
+        const file = e.target.files[0];
+        console.log(file);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(productData);
+    };
+
     return (
         <div className='max-w-5xl mx-auto p-6 shadow-md rounded-md'>
             <h2 className="text-3xl font-bold mb-6">Edit Product</h2>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 {/* Name */}
                 <div className='mb-6'>
                     <label className='block font-semibold mb-2'>Product Name</label>
@@ -48,7 +58,7 @@ const EditProductPage = () => {
                     <label className='block font-semibold mb-2'>Description</label>
                     <textarea
                         name="description" 
-                        value={productData.description} rows={4}
+                        value={productData.description} rows={4} onChange={handleChange}
                         className='w-full border border-gray-300 rounded-md p-2' required />
                 </div>
 
@@ -108,6 +118,21 @@ const EditProductPage = () => {
                         }
                         className='w-full border border-gray-300 rounded-md p-2' />
                 </div>
+
+                {/* Image to Upload */}
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">Upload Image</label>
+                    <input type="file" onChange={handleImageUpload} />
+                    <div className="flex gap-4 mt-4">
+                        {productData.images.map((image, index) => (
+                            <div key={index}>
+                                <img src={image.url} alt={image.altText || 'Product Image'} className='w-20 h-20 object-cover rounded-md shadow-md' />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <button type="submit" className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition-colors">Update Product</button>
             </form>
         </div>
     )
